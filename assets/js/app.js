297047ebfdd64f837de184e42e406a7d038eba2c
Peter Jones <peterjjones98@gmail.com>
2022-08-20 15:20:14 -0700

    Local camera streaming enabled

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,4 @@
-// We import the CSS which is extracted to its own file by esbuild.
-// Remove this line if you add a your own CSS build pipeline (e.g postcss).
 import "../css/app.css"
-
-// If you want to use Phoenix channels, run `mix help phx.gen.channel`
-// to get started and then uncomment the line below.
-// import "./user_socket.js"
-
-// You can include dependencies in two ways.
-//
-// The simplest option is to put them in assets/vendor and
-// import them using relative paths:
-//
-//     import "../vendor/some-package.js"
-//
-// Alternatively, you can `npm install some-package --prefix assets` and import
-// them using a path starting with the package name:
-//
-//     import "some-package"
-//
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
@@ -27,20 +8,45 @@ import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 
+async function initStream() {
+    try{
+        // Gets local media from browser and stores it in steam.
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+            width: "1280" });
+        
+        // Stores stream in global constant.
+        localSteam = stream
+        
+        // Sets local-video element to stream from webcam.
+        document.getElementById("local-video").srcObject = stream;
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+let Hooks = {}
+Hooks.JoinCall = {
+    mounted() {
+        initStream();
+    }
+}
+
+let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks, params: {_csrf_token: csrfToken}})
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"});
+// window.addEventListener("phx:page-loading-start", info => NProgress.start());
+window.addEventListener("phx:page-loading-start", info => topbar.show());
+window.addEventListener("phx:page-loading-stop", info => topbar.hide());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
-
+window.liveSocket = liveSocket;
